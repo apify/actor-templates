f5d311aaa06458250cce9bc7d9ed1f5315f2a30d
Jan Bárta <jbarta.edu@gmail.com>
2020-03-18 11:27:24 +0100

    constants changes

--- src/consts.js
+++ src/consts.js
@@ -4,14 +4,27 @@ const TEMPLATES_NAME = [
     'cheerio_crawler',
     'hello_world',
     'puppeteer_crawler',
-    'puppeteer_single_page'
+    'puppeteer_single_page',
+];
+
+const EXAMPLES_NAME = [
+    'map_and_reduce',
+    'forms',
+    'call_actor',
+    'puppeteer_sitemap',
+    'puppeteer_with_proxy',
+    'screenshots',
+    'synchronous_run'
 ];
 
 const BUILD_DIR_NAME = 'build';
 const TEMPLATES_DIR_NAME = 'templates';
+const EXAMPLES_DIR_NAME = '../examples';
 
 module.exports = {
     TEMPLATES_NAME,
     BUILD_DIR_NAME,
     TEMPLATES_DIR_NAME,
+    EXAMPLES_DIR_NAME,
+    EXAMPLES_NAME
 };
