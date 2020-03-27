38021417ff9c9f107450138029efb936f0154b1f
Jan Bárta <jbarta.edu@gmail.com>
2020-03-27 14:46:31 +0100

    Added puppeteer recursive crawl example

---
+++ examples/puppeteer_recursive_crawl/puppeteer_recursive_crawl.js
@@ -0,0 +1,25 @@
+const Apify = require("apify");
+
+Apify.main(async () => {
+    const requestQueue = await Apify.openRequestQueue();
+    await requestQueue.addRequest({ url: "https://www.iana.org/" });
+    const pseudoUrls = [new Apify.PseudoUrl("https://www.iana.org/[.*]")];
+
+    const crawler = new Apify.PuppeteerCrawler({
+        requestQueue,
+        handlePageFunction: async ({ request, page }) => {
+            const title = await page.title();
+            console.log(`Title of ${request.url}: ${title}`);
+            await Apify.utils.enqueueLinks({
+                page,
+                selector: "a",
+                pseudoUrls,
+                requestQueue
+            });
+        },
+        maxRequestsPerCrawl: 10,
+        maxConcurrency: 10
+    });
+
+    await crawler.run();
+});
---
+++ examples/puppeteer_recursive_crawl/puppeteer_recursive_crawl.md
@@ -0,0 +1,11 @@
+---
+id: puppeteer-recursive-crawl
+title: Puppeteer recursive crawl
+---
+
+Run the following example to perform a recursive crawl of a website using Puppeteer.
+
+ > To run this example on the Apify Platform, select the `Node.js 12 + Chrome on Debian (apify/actor-node-chrome)` 
+ >base image on the **Source** tab when configuring the actor.
+
+{{#code}}../../examples/puppeteer_recursive_crawl/puppeteer_recursive_crawl.js{{/code}}
