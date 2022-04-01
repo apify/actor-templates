77929b71b3a9c9a14bc8f2d191878b1f00975d4d
Ondra Urban <ondra@apify.com>
2022-04-01 16:42:54 +0200

    chore: remove outdated dependency

--- src/index.js
+++ src/index.js
@@ -1,14 +1,32 @@
-const httpRequest = require('@apify/http-request');
+const https = require('https');
 
-const MANIFEST_URL = 'https://github.com/apifytech/actor-templates/raw/master/templates/manifest.json';
+const MANIFEST_URL = 'https://raw.githubusercontent.com/apify/actor-templates/master/templates/manifest.json';
 
 exports.fetchManifest = async () => {
-    const { body } = await httpRequest({
-        url: MANIFEST_URL,
-        json: true,
-        throwOnHttpErrors: true,
-    });
-    return body;
+    return new Promise((resolve, reject) => {
+        https.get(MANIFEST_URL, (res) => {
+            let json = '';
+            res
+                .on('data', (chunk) => {
+                    json += chunk;
+                })
+                .on('end', () => {
+                    if (res.statusCode === 200) {
+                        try {
+                            const data = JSON.parse(json);
+                            resolve(data);
+                        } catch (e) {
+                            reject(e)
+                        }
+                    } else {
+                        reject(new Error(`Status: ${res.statusCode}\n${json}`));
+                    }
+                })
+                .on('error', (err) => reject(err))
+        }).on('error', (err) => reject(err));
+    })
 };
 
 exports.manifestUrl = MANIFEST_URL;
+
+exports.fetchManifest().then(console.log)
