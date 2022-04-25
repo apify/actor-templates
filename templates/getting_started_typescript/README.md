44eecb9713a3333d2c4139266ec97fc5c9b368d9
Ondra Urban <ondra@apify.com>
2022-04-25 17:20:14 +0200

    feat: getting started templates

---
+++ templates/getting_started_typescript/README.md
@@ -0,0 +1,19 @@
+# Getting started with Apify actors
+
+The `README.md` file documents what your actor does and how to use it,
+which is then displayed in the Console or Apify Store. It's always a good
+idea to write a `README.md`. In a few months, not even you
+will remember all the details about the actor.
+
+You can use [Markdown](https://www.markdownguide.org/cheat-sheet)
+language for rich formatting.
+
+## Documentation reference
+
+- [Apify SDK](https://sdk.apify.com/)
+- [Apify Actor documentation](https://docs.apify.com/actor)
+- [Apify CLI](https://docs.apify.com/cli)
+
+## Writing a README
+
+See our tutorial on [writing READMEs for your actors](https://help.apify.com/en/articles/2912548-how-to-write-great-readme-for-your-actors) if you need more inspiration.
---
+++ templates/getting_started_typescript/src/main.ts
@@ -0,0 +1,33 @@
+// This is the main Node.js source code file of your actor.
+// An actor is a program that takes an input and produces an output.
+
+// For more information, see https://sdk.apify.com/
+import Apify from 'apify';
+
+interface InputSchema {
+    firstNumber: number;
+    secondNumber: number;
+}
+
+Apify.main(async () => {
+    console.log('Loading input')
+    // Structure of input is defined in INPUT_SCHEMA.json.
+    const input = await Apify.getInput() as InputSchema;
+    console.log('First number: ', input.firstNumber);
+    console.log('Second number: ', input.secondNumber);
+
+    // 👉 Complete the code so that result is
+    // the sum of firstNumber and secondNumber.
+    // 👇👇👇👇👇👇👇👇👇👇
+    const result = ;
+    // 👆👆👆👆👆👆👆👆👆👆
+
+    console.log('The result is: ', result);
+
+    // Structure of output is defined in .actor/actor.json
+    await Apify.pushData({
+        firstNumber: input.firstNumber,
+        secondNumber: input.secondNumber,
+        sum: result,
+    })
+});
