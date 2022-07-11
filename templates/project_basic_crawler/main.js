77124db322b10986404bd65acc237d407b3c63d9
Eneiro Matos <eneiromatos@gmail.com>
2022-07-11 01:29:05 -0400

    Use Apify.BasicCrawler and handleRequestFunction just pass the request to the handler functions.

--- templates/project_basic_crawler/main.js
+++ templates/project_basic_crawler/main.js
@@ -4,40 +4,44 @@
  * If you're looking for examples or want to learn more, see README.
  */
 
-const Apify = require('apify');
-const { handleStart, handleList, handleDetail } = require('./src/routes');
+const Apify = require("apify");
+const { handleStart, handleList, handleDetail } = require("./src/routes");
 
-const { utils: { log } } = Apify;
+const {
+    utils: { log },
+} = Apify;
 
 Apify.main(async () => {
     const { startUrls } = await Apify.getInput();
 
-    const requestList = await Apify.openRequestList('start-urls', startUrls);
+    const requestList = await Apify.openRequestList("start-urls", startUrls);
     const requestQueue = await Apify.openRequestQueue();
-    const proxyConfiguration = await Apify.createProxyConfiguration();
 
-    const crawler = new Apify.CheerioCrawler({
+    const proxyConfiguration = await Apify.createProxyConfiguration();
+    global.proxyUrl = proxyConfiguration.newUrl();
+
+    const crawler = new Apify.BasicCrawler({
         requestList,
         requestQueue,
-        proxyConfiguration,
         // Be nice to the websites.
         // Remove to unleash full power.
         maxConcurrency: 50,
-        handlePageFunction: async (context) => {
-            const { url, userData: { label } } = context.request;
-            log.info('Page opened.', { label, url });
+        maxRequestRetries: 3,
+        handleRequestFunction: async ({ request }) => {
+            const {
+                userData: { label },
+            } = request;
             switch (label) {
-                case 'LIST':
-                    return handleList(context);
-                case 'DETAIL':
-                    return handleDetail(context);
+                case "LIST":
+                    return handleList(request);
+                case "DETAIL":
+                    return handleDetail(request);
                 default:
-                    return handleStart(context);
+                    return handleStart(request);
             }
         },
     });
-
-    log.info('Starting the crawl.');
+    log.info("Starting the crawl.");
     await crawler.run();
-    log.info('Crawl finished.');
+    log.info("Crawl finished.");
 });
