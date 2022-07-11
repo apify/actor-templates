18ed7ef69662a3d688795562e8f1c6aee14cb711
Eneiro Matos <eneiromatos@gmail.com>
2022-07-11 01:33:06 -0400

    Add requestAsBrowser in each handler function to do the requests.

--- templates/project_basic_crawler/src/routes.js
+++ templates/project_basic_crawler/src/routes.js
@@ -1,15 +1,35 @@
-const Apify = require('apify');
+const Apify = require("apify");
 
-const { utils: { log } } = Apify;
+const {
+    utils: { log },
+} = Apify;
 
-exports.handleStart = async ({ request, $ }) => {
+exports.handleStart = async ({ url, label }) => {
     // Handle Start URLs
+    const { body, statusCode } = await Apify.utils.requestAsBrowser({
+        url,
+        proxyUrl,
+    });
+    log.info("Page opened.", { label, url, statusCode});
+    // start your code here using body for parsing data with your favorite libraries.
 };
 
-exports.handleList = async ({ request, $ }) => {
+exports.handleList = async ({ url }) => {
     // Handle pagination
+    const { body, statusCode } = await Apify.utils.requestAsBrowser({
+        url,
+        proxyUrl,
+    });
+    log.info("Page loaded: ", { url, statusCode, label });
+    // start your code here using body for parsing data with your favorite libraries.
 };
 
-exports.handleDetail = async ({ request, $ }) => {
+exports.handleDetail = async ({ url }) => {
     // Handle details
+    const { body, statusCode } = await Apify.utils.requestAsBrowser({
+        url,
+        proxyUrl,
+    });
+    log.info("Page loaded: ", { url, statusCode, label });
+    // start your code here using body for parsing data with your favorite libraries.
 };
