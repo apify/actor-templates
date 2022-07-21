2e07509b043ec8c9ba0f9d9cb2dea6193b209ed7
Vlad Frangu <kingdgrizzle@gmail.com>
2022-07-21 16:59:08 +0300

    chore: move sdk v2 examples to new repo

--- examples/accept_user_input/accept_user_input.js
+++
@@ -1,6 +0,0 @@
-const { Actor } = require('apify');
-
-Actor.main(async () => {
-    const input = await Actor.getInput();
-    console.log(input);
-});
--- src/build.js
+++ src/build.js
@@ -1,13 +1,8 @@
 const { buildTemplates } = require('./build_templates');
-const { buildExamples } = require('./build_examples');
 
 // run build
 (async () => {
     // build templates
     await buildTemplates();
     console.log('Templates zips were created!');
-
-    // build examples
-    await buildExamples();
-    console.log('Example markdowns were created!');
 })();
