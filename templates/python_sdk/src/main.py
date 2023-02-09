8327016c7c279de633802121fea0f56df31643b4
František Nesveda <frantisek@apify.com>
2023-02-09 15:22:27 +0100

    Add Python SDK beta template

---
+++ templates/python_sdk/src/main.py
@@ -0,0 +1,31 @@
+from apify import Actor
+
+
+async def main():
+    async with Actor() as actor:
+        # Get the value of the actor input
+        actor_input = await actor.get_input() or {}
+
+        # Structure of input is defined in INPUT_SCHEMA.json
+        first_number = actor_input.get('first_number')
+        second_number = actor_input.get('second_number')
+
+        print(f'First number: {first_number}')
+        print(f'Second number: {second_number}')
+
+        # 👉 Complete the code so that result is
+        # the sum of first_number and second_number.
+        # 👇👇👇👇👇👇👇👇👇👇
+        result = None
+        # 👆👆👆👆👆👆👆👆👆👆
+
+        print(f'The result is: {result}')
+
+        # Structure of output is defined in .actor/actor.json
+        await actor.push_data([
+            {
+                'first_number': first_number,
+                'second_number': second_number,
+                'sum': result,
+            },
+        ])
