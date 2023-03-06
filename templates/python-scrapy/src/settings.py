7db8d21afa6b6a8542faf11f8b3783255693653b
František Nesveda <frantisek@apify.com>
2023-03-06 14:05:00 +0100

    Add a Python + Scrapy template

---
+++ templates/python-scrapy/src/settings.py
@@ -0,0 +1,18 @@
+# Scrapy settings for this project
+#
+# For simplicity, this file contains only settings considered important or
+# commonly used. You can find more settings consulting the documentation:
+#
+#     http://doc.scrapy.org/en/latest/topics/settings.html
+
+
+BOT_NAME = 'quotesbot'
+
+SPIDER_MODULES = ['src.spiders']
+NEWSPIDER_MODULE = 'src.spiders'
+
+REQUEST_FINGERPRINTER_IMPLEMENTATION = '2.7'
+TWISTED_REACTOR = 'twisted.internet.asyncioreactor.AsyncioSelectorReactor'
+
+# Obey robots.txt rules
+ROBOTSTXT_OBEY = True
