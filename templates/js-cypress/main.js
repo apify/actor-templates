831931f6b4750e60038bf6f9739d633cdeb10253
Katerina Hronikova <katacek@gmail.com>
2023-03-10 16:03:32 +0100

    pr review fixes

--- templates/js-cypress/main.js
+++ templates/js-cypress/main.js
@@ -2,42 +2,35 @@ import { Actor } from 'apify';
 import fs from 'fs';
 import cypress from 'cypress';
 import { globby } from 'globby';
+import log from '@apify/log';
 
 await Actor.init();
 
 // todo: be able to send everything in input
-const { video, defaultCommandTimeout, viewportHeight, viewportWidth, ...rest } = await Actor.getInput();
+const input = await Actor.getInput();
 
 const runOneSpec = (spec) => {
 return cypress.run({
-    config: {
-        video,
-        defaultCommandTimeout,
-        viewportHeight,
-        viewportWidth,
-        ...rest,
-    },
+    config: { input },
     spec,
 })
 }
 
 const tests = await globby('./cypress/e2e/*-spec.cy.js');
 
-console.log(`Running tests from following files: ${tests}`)
+log.info(`Running tests from following files: ${tests}`)
 
 const kvs = await Actor.openKeyValueStore();
 const dataset = await Actor.openDataset();
 for (const test of tests) {
     const result = await runOneSpec(test);
-    let keyValueStoreLink = '';
+    let keyValueStoreLink;
     if (result.config.video) {
         const baseName = test.split('/').pop();
         const file = `./cypress/videos/${baseName}.mp4`;
         const kvsKeyName = baseName.replaceAll('.', '-');
         await kvs.setValue(kvsKeyName, fs.readFileSync(file), { contentType: 'video/mp4' });
-
         keyValueStoreLink = await kvs.getPublicUrl(kvsKeyName);
-        result.keyValueStoreLink = keyValueStoreLink;
     }
     const transformedResult = {
         testSuiteTitle: result.runs[0].tests[0].title[0],
@@ -46,7 +39,7 @@ for (const test of tests) {
         totalFailed: result.totalFailed,
         totalSkipped: result.totalSkipped,
 		totalDuration: result.totalDuration,
-        videoLink: result.keyValueStoreLink,
+        videoLink: keyValueStoreLink,
         rawData: result,
     }
     await dataset.pushData(transformedResult);
