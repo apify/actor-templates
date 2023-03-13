07cd0d4ae45faa8ce94d63953123dc841235837b
Katerina Hronikova <katacek@gmail.com>
2023-03-13 15:48:20 +0100

    final touches

--- templates/js-cypress/cypress.config.js
+++ templates/js-cypress/cypress.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'cypress';
 
 export default defineConfig({
     e2e: {
+        // to be able to run the tests locally
         baseUrl: 'https://apify.com',
     },
 });
