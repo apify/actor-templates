0291ab96291c79147fa56272e949165625d8d7f1
František Nesveda <fnesveda@users.noreply.github.com>
2023-03-14 09:55:55 +0100

    Post-create messages for Python actors (#135)
    Python actors require some post-create instructions:
    - how to add additonal dependencies
    - how to install Playwright and its browsers
    
    This PR adds thse post-create messages with specific instructions to the
    Python actor templates.
    
    The accompanying PR for Apify CLI is here:
    https://github.com/apify/apify-cli/pull/349

--- templates/python-playwright/src/main.py
+++ templates/python-playwright/src/main.py
@@ -4,7 +4,7 @@ from apify import Actor
 from playwright.async_api import async_playwright
 
 # To run this Actor locally, you need to have the Playwright browsers installed.
-# Run `playwright install` in the Actor's virtual environment to install them.
+# Run `playwright install --with-deps` in the Actor's virtual environment to install them.
 # When running on the Apify platform, they are already included in the Actor's Docker image.
 
 
