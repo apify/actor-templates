132808a4265f0bb2240f9f7633b5e7e16b4c34ec
Marek Trunkát <marek@trunkat.eu>
2023-05-05 15:16:17 +0200

    Adding comments

--- templates/js-langchain/src/main.js
+++ templates/js-langchain/src/main.js
@@ -9,12 +9,16 @@ import { retrieveVectorIndex, cacheVectorIndex } from './vector_index_cache.js';
 
 await Actor.init();
 
-// There are 2 environment variables you need to configure.
+// There are 2 environment variables you need to configure (https://docs.apify.com/cli/docs/vars#set-up-environment-variables-in-apify-console):
 // 1. Apify API token you obtain at https://console.apify.com/account/integrations. This is not nessesary when running this Actor at Apify platform.
 // 2. OpenAI API key you obtain at https://platform.openai.com/account/api-keys.
 const { OPENAI_API_KEY, APIFY_API_TOKEN } = process.env;
 
+// Local directory where the vector index will be stored.
 const VECTOR_INDEX_PATH = './vector_index';
+
+// Enforce a re-scrape of website content and re-creation of the vector index.
+const FORCE_RESCRAPE = false;
 
 if (!OPENAI_API_KEY || !OPENAI_API_KEY.length) throw new Error('Please configure the OPENAI_API_KEY environment variable!');
 if (!APIFY_API_TOKEN || !APIFY_API_TOKEN.length) throw new Error('Please configure the APIFY_API_TOKEN environment variable!');
@@ -22,19 +26,19 @@ if (!APIFY_API_TOKEN || !APIFY_API_TOKEN.length) throw new Error('Please configu
 const model = new OpenAI({ openAIApiKey: OPENAI_API_KEY });
 const apify = new ApifyWrapper(APIFY_API_TOKEN);
 
-// Then run the Actor, wait for it to finish, and fetch its results from the Apify dataset into a LangChain document loader.
-// Note that if you already have some results in an Apify dataset, you can load them directly using `ApifyDatasetLoader`, 
-// as shown in [this guide](../../../indexes/document_loaders/examples/web_loaders/apify_dataset.md). 
-// In that guide, you'll also find the explanation of the `datasetMappingFunction`, 
-// which is used to map fields from the Apify dataset records to LangChain `Document` fields.
+// Now we want to creare a vector index from the crawled documents. 
+// Following object represents an input for the https://apify.com/apify/website-content-crawler actor that scrapes the website.
 const websiteContentCrawlerInput = {
     startUrls: [{ url: 'https://js.langchain.com/docs' }],
-    maxCrawlPages: 30,
+    maxCrawlPages: 1,
 };
 
+// First, we check if the vector index is already cached. If not, we run the website content crawler to get the documents.
+// By setting up FORCE_RESCRAPE=true you can enforce a re-scrape of the website content and re-creation of the vector index.
 console.log('Fetching cached vector index from key-value store...');
-const wasFetched = await retrieveVectorIndex(websiteContentCrawlerInput);
-if (!wasFetched) {
+const reinitializeIndex = FORCE_RESCRAPE || !(await retrieveVectorIndex(websiteContentCrawlerInput));
+if (reinitializeIndex) {
+    // Run the Actor, wait for it to finish, and fetch its results from the Apify dataset into a LangChain document loader.
     console.log('Vector index not found.')
     console.log('Running apify/website-content-crawler...');
     const loader = await apify.callActor(
@@ -47,14 +51,14 @@ if (!wasFetched) {
     );
     const docs = await loader.load();
 
-    // Initialize the vector index from the crawled documents:
+    // Initialize the vector index from the crawled documents.
     console.log('Feeding vector index with crawling results...');
     const vectorStore = await HNSWLib.fromDocuments(
         docs,
         new OpenAIEmbeddings({ openAIApiKey: OPENAI_API_KEY })
     );
 
-    // Save the vector index to the key-value store.
+    // Save the vector index to the key-value store so that we can skip this phase in the next run.
     console.log('Saving vector index to the disk...')
     await vectorStore.save(VECTOR_INDEX_PATH);
     await cacheVectorIndex(websiteContentCrawlerInput, VECTOR_INDEX_PATH);
--- templates/js-langchain/src/vector_index_cache.js
+++ templates/js-langchain/src/vector_index_cache.js
@@ -3,30 +3,54 @@ import { createHash } from 'crypto';
 import { pipeline } from 'node:stream/promises';
 import tar from 'tar';
 
+const VECTOR_INDEX_CACHE_STORE_NAME = 'vector-index-cache';
+
 const client = Actor.newClient();
 
+/**
+ * Generates vector index cache key as a hash of provided configuration object.
+ *
+ * @param {Object} config
+ * @returns {String}
+ */
 function getIndexCacheKey(config) {
     const hash = createHash('md5').update(JSON.stringify(config)).digest('hex');
 
     return `${hash}.tar.gz`;
 }
 
+/**
+ * Returns an ID of a key-value store with a name VECTOR_INDEX_CACHE_STORE_NAME.
+ * 
+ * @returns {String}
+ */
 async function getVectorIndexCacheStore() {
-    const { id: vectorIndexCacheStoreId } = await client.keyValueStores().getOrCreate('vector-index-cache');
+    const { id: vectorIndexCacheStoreId } = await client.keyValueStores().getOrCreate(VECTOR_INDEX_CACHE_STORE_NAME);
 
     return client.keyValueStore(vectorIndexCacheStoreId);
 }
 
+/**
+ * Caches a vector index from `indexPath` in a VECTOR_INDEX_CACHE_STORE_NAME key-value store a key generated from `config`.
+ *
+ * @param {Object} config
+ * @param {String} indexPath
+ */
 export async function cacheVectorIndex(config, indexPath) {
     const vectorIndexCacheStore = await getVectorIndexCacheStore();
     const gzipedVectorIndexStream = tar.c({ gzip: true }, [indexPath]);
-    console.log(typeof gzipedVectorIndexStream);
-    await vectorIndexCacheStore.setRecord({ 
-        key: getIndexCacheKey(config), 
+    await vectorIndexCacheStore.setRecord({
+        key: getIndexCacheKey(config),
         value: gzipedVectorIndexStream,
     });
 }
 
+/**
+ * Fetches a cached vector index from a VECTOR_INDEX_CACHE_STORE_NAME key-value store and extracts it to the current directory.
+ *
+ * @param {Object} config
+ * @returns {Boolean} noting if the vector index was found in the cache
+ */
 export async function retrieveVectorIndex(config) {
     const vectorIndexCacheStore = await getVectorIndexCacheStore();
     let vectorIndexRecord;
