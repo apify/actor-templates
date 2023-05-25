a2b654083cd60c9a44bd3a162f81762e97eb6549
HonzaTuron <jan.turon@apify.com>
2023-05-25 13:48:35 +0200

    simplify template

--- templates/python-start-scrape-single-page/src/main.py
+++ templates/python-start-scrape-single-page/src/main.py
@@ -10,26 +10,15 @@ async def main():
         actor_input = await Actor.get_input() or {}
         url = actor_input.get('start_url')
 
-        # Enqueue the starting URLs in the default request queue
-        default_queue = await Actor.open_request_queue()
-        await default_queue.add_request({ 'url': url, 'userData': { 'depth': 0 }})
+        Actor.log.info(f'Scraping {url}...')
 
-        # Process the requests in the queue one by one
-        while request := await default_queue.fetch_next_request():
-            url = request['url']
-            depth = request['userData']['depth']
-            Actor.log.info(f'Scraping {url} ...')
+        try:
+            # Fetch the URL using `requests` and parse it using `BeautifulSoup`
+            response = requests.get(url)
+            soup = BeautifulSoup(response.content, 'html.parser')
 
-            try:
-                # Fetch the URL using `requests` and parse it using `BeautifulSoup`
-                response = requests.get(url)
-                soup = BeautifulSoup(response.content, 'html.parser')
-
-                # Push the title of the page into the default dataset
-                title = soup.title.string if soup.title else None
-                await Actor.push_data({ 'url': url, 'title': title })
-            except:
-                Actor.log.exception(f'Cannot extract data from {url}.')
-            finally:
-                # Mark the request as handled so it's not processed again
-                await default_queue.mark_request_as_handled(request)
+            # Push the title of the page into the default dataset
+            title = soup.title.string if soup.title else None
+            await Actor.push_data({ 'url': url, 'title': title })
+        except:
+            Actor.log.exception(f'Cannot extract data from {url}.')
