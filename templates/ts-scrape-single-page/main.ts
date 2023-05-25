167d9712d005bdab5be36ccead2f66634b05a331
Dan Nguyen <nguyendan.denky@gmail.com>
2023-05-25 11:34:12 +0200

    udpate main

--- templates/ts-scrape-single-page/main.ts
+++ templates/ts-scrape-single-page/main.ts
@@ -1,55 +1,40 @@
-// Axios import - Promise based HTTP client for the browser and node.js (Read more at https://axios-http.com/docs/intro)
+// Axios - Promise based HTTP client for the browser and node.js (Read more at https://axios-http.com/docs/intro).
 import axios from "axios";
-
-// Cheerio - The fast, flexible & elegant library for parsing and manipulating HTML and XML (Read more at https://cheerio.js.org/)
-import * as cheerio from 'cheerio';
-
-// Apify SDK - toolkit for building Apify Actors (Read more at https://docs.apify.com/sdk/js/)
+// Cheerio - The fast, flexible & elegant library for parsing and manipulating HTML and XML (Read more at https://cheerio.js.org/).
+import * as cheerio from "cheerio";
+// Apify SDK - toolkit for building Apify Actors (Read more at https://docs.apify.com/sdk/js/).
 import { Actor } from "apify";
 
-// Initialize the Apify SDK
+// The init() call configures the Actor for its environment. It's recommended to start every Actor with an init().
 await Actor.init();
 
 interface Input {
     url: string;
 }
-
-// Structure of input is defined in input_schema.json.
+// Structure of input is defined in input_schema.json
 const input = await Actor.getInput<Input>();
+if(!input) throw new Error("Input is missing!");
 
-if(!input) throw new Error('Input is missing!');
+const { url } = input
+// Fetch the HTML content of the page.
+const response = await axios.get(url);
 
-// Getting the URL from input defined in ./storage/key_value_stores/default/INPUT.json
-const { url } = input;
+// Parse the downloaded HTML with Cheerio to enable data extraction.
+const $ = cheerio.load(response.data);
 
-interface Heading {
-    level: string,
-    text: string
-}
-const headings : Heading[] = [];
+// Extract all headings from the page (tag name and text).
+const headings : { level: string, text: string }[] = [];
+$("h1, h2, h3, h4, h5, h6").each((i, element) => {
+    const headingObject = {
+        level: $(element).prop("tagName").toLowerCase(),
+        text: $(element).text(),
+    };
+    console.log("Extracted heading", headingObject);
+    headings.push(headingObject);
+});
 
-try {
-    // Fetching the HTML content of the page
-    const response = await axios.get(url);
-
-    // Load the HTML to cheerio
-    const $ = cheerio.load(response.data);
-
-    // Extracting all the headings from the page (tag name and text)
-    $('h1, h2, h3, h4, h5, h6').each((i, element) => {
-        const headingObject = {
-            level: $(element).prop("tagName").toLowerCase(),
-            text: $(element).text(),
-        };
-        console.log('Extracted heading', headingObject);
-        headings.push(headingObject);
-    });
-} catch (error) {
-    console.error(error);
-}
-
-// Pushing the data to dataset
+// Save headings to Dataset - a table-like storage.
 await Actor.pushData(headings);
 
-// Exit successfully
+// Gracefully exit the Actor process. It's recommended to quit all Actors with an exit().
 await Actor.exit();
