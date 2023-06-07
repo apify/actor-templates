a21c341527b758b597a83469899b39b158a34bfa
Dan Nguyen <nguyendan.denky@gmail.com>
2023-06-07 10:31:29 +0200

    lint fix

--- src/index.js
+++ src/index.js
@@ -1,31 +1,31 @@
 const https = require('https');
 
 const MANIFEST_URL = 'https://raw.githubusercontent.com/apify/actor-templates/master/templates/manifest.json';
-const CONSOLE_README_SUFFIX_URL = "https://raw.githubusercontent.com/apify/actor-templates/master/templates/consoleReadmeSuffix.md"
-const LOCAL_README_SUFFIX_URL ="https://raw.githubusercontent.com/apify/actor-templates/master/templates/localReadmeSuffix.md"
+const CONSOLE_README_SUFFIX_URL = 'https://raw.githubusercontent.com/apify/actor-templates/master/templates/consoleReadmeSuffix.md';
+const LOCAL_README_SUFFIX_URL = 'https://raw.githubusercontent.com/apify/actor-templates/master/templates/localReadmeSuffix.md';
 
 /**
  * This will fetches the readme suffixes for the console and local templates.
  * These suffixes are then added to the manifest object at the root level
  */
 async function fetchReadmeSuffixes() {
-    let consoleReadmeSuffix = "";
-    let localReadmeSuffix = "";
+    let consoleReadmeSuffix = '';
+    let localReadmeSuffix = '';
 
     try {
         const consoleResponse = await fetch(CONSOLE_README_SUFFIX_URL);
         const localResponse = await fetch(LOCAL_README_SUFFIX_URL);
         consoleReadmeSuffix = await consoleResponse.text();
         localReadmeSuffix = await localResponse.text();
-    } catch(error) {
+    } catch (error) {
         console.log(error);
     }
 
-    return { consoleReadmeSuffix, localReadmeSuffix }
+    return { consoleReadmeSuffix, localReadmeSuffix };
 }
 
 exports.fetchManifest = async () => {
-    const { consoleReadmeSuffix,localReadmeSuffix } = await fetchReadmeSuffixes();
+    const { consoleReadmeSuffix, localReadmeSuffix } = await fetchReadmeSuffixes();
 
     return new Promise((resolve, reject) => {
         https.get(MANIFEST_URL, (res) => {
