d8f9ad808d984b5e69f9f75f1ccae79d0a0f44c6
Dan Nguyen <nguyendan.denky@gmail.com>
2023-06-08 12:41:18 +0200

    lint fix

--- src/index.js
+++ src/index.js
@@ -4,33 +4,29 @@ const MANIFEST_URL = 'https://raw.githubusercontent.com/apify/actor-templates/ma
 const CONSOLE_README_SUFFIX_URL = 'https://raw.githubusercontent.com/apify/actor-templates/master/templates/consoleReadmeSuffix.md';
 const LOCAL_README_SUFFIX_URL = 'https://raw.githubusercontent.com/apify/actor-templates/master/templates/localReadmeSuffix.md';
 
-
 exports.fetchManifest = async () => {
     try {
         const manifestResponse = await fetch(MANIFEST_URL);
 
-        if(manifestResponse.status !== 200) throw new Error(`Could not fetch manifest from ${MANIFEST_URL}`);
+        if (manifestResponse.status !== 200) throw new Error(`Could not fetch manifest from ${MANIFEST_URL}`);
 
         const manifestData = await manifestResponse.json();
         const consoleResponse = await fetch(CONSOLE_README_SUFFIX_URL);
         const localResponse = await fetch(LOCAL_README_SUFFIX_URL);
 
-        if(consoleResponse.status === 200) {
+        if (consoleResponse.status === 200) {
             manifestData.consoleReadmeSuffix = await consoleResponse.text();
         }
 
-        if(localResponse.status === 200) {
+        if (localResponse.status === 200) {
             manifestData.localReadmeSuffix = await localResponse.text();
         }
 
         return manifestData;
-    }
-
-    catch (err) {
+    } catch (err) {
         console.error(err);
         throw err;
     }
-
 };
 
 exports.manifestUrl = MANIFEST_URL;
