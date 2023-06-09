c73d4fe697fc7ddb141012a424d17be09546c4d1
Dan Nguyen <nguyendan.denky@gmail.com>
2023-06-09 11:36:17 +0200

    remove redundant try catch

--- src/index.js
+++ src/index.js
@@ -5,28 +5,23 @@ const CONSOLE_README_SUFFIX_URL = 'https://raw.githubusercontent.com/apify/actor
 const LOCAL_README_SUFFIX_URL = 'https://raw.githubusercontent.com/apify/actor-templates/master/templates/localReadmeSuffix.md';
 
 exports.fetchManifest = async () => {
-    try {
-        const manifestResponse = await fetch(MANIFEST_URL);
+    const manifestResponse = await fetch(MANIFEST_URL);
 
-        if (manifestResponse.status !== 200) throw new Error(`Could not fetch manifest from ${MANIFEST_URL}`);
+    if (manifestResponse.status !== 200) throw new Error(`Could not fetch manifest from ${MANIFEST_URL}`);
 
-        const manifestData = await manifestResponse.json();
-        const consoleResponse = await fetch(CONSOLE_README_SUFFIX_URL);
-        const localResponse = await fetch(LOCAL_README_SUFFIX_URL);
+    const manifestData = await manifestResponse.json();
+    const consoleResponse = await fetch(CONSOLE_README_SUFFIX_URL);
+    const localResponse = await fetch(LOCAL_README_SUFFIX_URL);
 
-        if (consoleResponse.status === 200) {
-            manifestData.consoleReadmeSuffix = await consoleResponse.text();
-        }
-
-        if (localResponse.status === 200) {
-            manifestData.localReadmeSuffix = await localResponse.text();
-        }
-
-        return manifestData;
-    } catch (err) {
-        console.error(err);
-        throw err;
+    if (consoleResponse.status === 200) {
+        manifestData.consoleReadmeSuffix = await consoleResponse.text();
     }
+
+    if (localResponse.status === 200) {
+        manifestData.localReadmeSuffix = await localResponse.text();
+    }
+
+    return manifestData;
 };
 
 exports.manifestUrl = MANIFEST_URL;
