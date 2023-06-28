15ea243d281709d21cac79f51224ad36240b8e2f
Jan Bárta <jbarta.edu@gmail.com>
2023-06-28 11:41:53 +0200

    fix: Unified the main files look and behavior for empty templates

--- templates/ts-empty/src/main.ts
+++ templates/ts-empty/src/main.ts
@@ -1,12 +1,9 @@
-// This is the main Node.js source code file of your actor.
-// An actor is a program that takes an input and produces an output.
-
 // Apify SDK - toolkit for building Apify Actors (Read more at https://docs.apify.com/sdk/js/)
 import { Actor } from "apify";
-//  Crawlee - web scraping and browser automation library (Read more at https://crawlee.dev)
+// Crawlee - web scraping and browser automation library (Read more at https://crawlee.dev)
 // import { CheerioCrawler } from 'crawlee';
 
-// Initialize the Apify SDK
+// The init() call configures the Actor for its environment. It's recommended to start every Actor with an init()
 await Actor.init();
 
 console.log('Hello from the Actor!');
@@ -14,5 +11,5 @@ console.log('Hello from the Actor!');
  * Actor code
  */
 
-// Exit successfully
+// Gracefully exit the Actor process. It's recommended to quit all Actors with an exit()
 await Actor.exit();
