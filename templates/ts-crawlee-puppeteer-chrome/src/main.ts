41092102d3b6b92fe75563ab90a277f413c2b632
Dan Nguyen <nguyendan.denky@gmail.com>
2023-06-30 17:17:07 +0200

    added aditional comments

--- templates/ts-crawlee-puppeteer-chrome/src/main.ts
+++ templates/ts-crawlee-puppeteer-chrome/src/main.ts
@@ -1,34 +1,30 @@
-/**
- * This template is a production ready boilerplate for developing with `CheerioCrawler`.
- * Use this to bootstrap your projects using the most up-to-date code.
- * If you're looking for examples or want to learn more, see README.
- */
-
-// For more information, see https://docs.apify.com/sdk/js
+// Apify SDK - toolkit for building Apify Actors (Read more at https://docs.apify.com/sdk/js/).
 import { Actor } from 'apify';
-// For more information, see https://crawlee.dev
+// Web scraping and browser automation library (Read more at https://crawlee.dev)
 import { PuppeteerCrawler, Request } from 'crawlee';
 import { router } from './routes.js';
 
-// Initialize the Apify SDK
+// The init() call configures the Actor for its environment. It's recommended to start every Actor with an init().
 await Actor.init();
 
 interface Input {
     startUrls: Request[];
 }
-// Structure of input is defined in input_schema.json
+// Define the URLs to start the crawler with - get them from the input of the Actor or use a default list.
 const input = await Actor.getInput<Input>();
-if (!input) throw new Error('Input is missing!');
-const { startUrls } = input;
+const startUrls = input?.startUrls || [{ url: 'https://apify.com' }];
 
+// Create a proxy configuration that will rotate proxies from Apify Proxy.
 const proxyConfiguration = await Actor.createProxyConfiguration();
 
+// Create a PuppeteerCrawler that will use the proxy configuration and and handle requests with the router from routes.js file.
 const crawler = new PuppeteerCrawler({
     proxyConfiguration,
     requestHandler: router,
 });
 
+// Run the crawler with the start URLs and wait for it to finish.
 await crawler.run(startUrls);
 
-// Exit successfully
+// Gracefully exit the Actor process. It's recommended to quit all Actors with an exit().
 await Actor.exit();
