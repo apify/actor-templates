154bd4278772c1c6e6913018b8ee6aab5b35d11f
nguyeda1 <nguyendan.denky@gmail.com>
2023-07-11 14:49:14 +0200

    moved default input

--- templates/js-cypress/cypress.config.js
+++ templates/js-cypress/cypress.config.js
@@ -1,8 +1,5 @@
 import { defineConfig } from 'cypress';
 
 export default defineConfig({
-    e2e: {
-        // to be able to run the tests locally
-        baseUrl: 'https://apify.com',
-    },
+    e2e: {},
 });
