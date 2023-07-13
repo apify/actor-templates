812210c38007ee6d65172fbc3fb9b2d8dd1b250f
nguyeda1 <nguyendan.denky@gmail.com>
2023-07-13 13:22:15 +0200

    revert the cypress default config

--- templates/js-cypress/cypress.config.js
+++ templates/js-cypress/cypress.config.js
@@ -1,5 +1,8 @@
 import { defineConfig } from 'cypress';
 
 export default defineConfig({
-    e2e: {},
+    e2e: {
+        // to be able to run the tests locally for Apify CLI versions 17.0.0 and older
+        baseUrl: 'https://apify.com',
+    },
 });
