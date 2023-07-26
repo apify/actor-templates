5afb64dd027b2fedf32ba9bfa82f77b546672af4
HonzaTuron <jan.turon@apify.com>
2023-07-26 15:09:08 +0200

    feat(app): cheerio js readme + code

--- templates/js-crawlee-cheerio/src/main.js
+++ templates/js-crawlee-cheerio/src/main.js
@@ -7,19 +7,27 @@
 // For more information, see https://docs.apify.com/sdk/js
 import { Actor } from 'apify';
 // For more information, see https://crawlee.dev
-import { CheerioCrawler } from 'crawlee';
-import { router } from './routes.js';
+import { CheerioCrawler, Dataset } from 'crawlee';
 
 // Initialize the Apify SDK
 await Actor.init();
 
-const startUrls = ['https://apify.com'];
+const { startUrls, maxRequestsPerCrawl } = await Actor.getInput() || { startUrls: ["https://crawlee.com"], maxRequestsPerCrawl: 100 };
 
 const proxyConfiguration = await Actor.createProxyConfiguration();
 
 const crawler = new CheerioCrawler({
     proxyConfiguration,
-    requestHandler: router,
+    maxRequestsPerCrawl,
+    requestHandler: async ({ enqueueLinks, request, $, log }) => {
+        log.info('enqueueing new URLs');
+        await enqueueLinks( { globs: ['https://crawlee.com/*'], label: 'detail' });
+
+        const title = $('title').text();
+        log.info(`${title}`, { url: request.loadedUrl });
+
+        await Dataset.pushData({ url: request.loadedUrl, title });
+    },
 });
 
 await crawler.run(startUrls);
