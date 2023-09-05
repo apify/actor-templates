8aef1341cbaf601b4ef3fff39328c0b5b9a99b1f
Vlada Dusek <v.dusek96@gmail.com>
2023-09-05 17:53:37 +0200

    Fix middleware

--- templates/python-scrapy/src/middlewares.py
+++ templates/python-scrapy/src/middlewares.py
@@ -1,11 +1,15 @@
+import asyncio
+import traceback
+
 from scrapy import Spider
 from scrapy.downloadermiddlewares.retry import RetryMiddleware
 from scrapy.http import Request, Response
 from scrapy.utils.response import response_status_message
 
 from apify import Actor
+from apify.storages import RequestQueue
 
-from .event_loop_manager import EventLoopManager, get_running_event_loop_id
+from .event_loop_management import get_running_event_loop_id, open_queue_with_custom_client
 
 
 class ApifyRetryMiddleware(RetryMiddleware):
@@ -14,8 +18,18 @@ class ApifyRetryMiddleware(RetryMiddleware):
     """
 
     def __init__(self, *args: list, **kwargs: dict) -> None:
-        Actor.log.debug('ApifyMiddleware initializing...')
+        Actor.log.debug(f'[{get_running_event_loop_id()}] ApifyRetryMiddleware initializing...')
         super().__init__(*args, **kwargs)
+        self._event_loop: asyncio.AbstractEventLoop = asyncio.new_event_loop()
+        try:
+            self._rq: RequestQueue = self._event_loop.run_until_complete(open_queue_with_custom_client())
+        except BaseException:
+            traceback.print_exc()
+
+    def __del__(self):
+        Actor.log.debug('ApifyRetryMiddleware is being deleted...')
+        self._event_loop.stop()
+        self._event_loop.close()
 
     def process_response(self, request: Request, response: Response, spider: Spider) -> Request | Response:
         """
@@ -29,28 +43,46 @@ class ApifyRetryMiddleware(RetryMiddleware):
         Returns:
             The response, or a new request if the request should be retried.
         """
-        Actor.log.debug(f'[{get_running_event_loop_id()}] ApifyMiddleware is processing: {request}, {response}...')
+        Actor.log.debug(f'[{get_running_event_loop_id()}] ApifyRetryMiddleware is processing: {request}, {response}...')
 
-        apify_request = {
-            'id': request.headers.get('x-apify-request-id'),
-            'uniqueKey': request.headers.get('x-apify-request-uniquekey'),
-        }
-
-        async def handle_retry_logic():
-            Actor.log.debug(f'[{get_running_event_loop_id()}] ApifyMiddleware.handle_retry_logic is called...')
-            rq = await Actor.open_request_queue()
-
-            if request.meta.get("dont_retry", False):
-                rq.mark_request_as_handled(apify_request)
-                return response
-
-            if response.status in self.retry_http_codes:
-                rq.reclaim_request(apify_request)
-                reason = response_status_message(response.status)
-                return self._retry(request, reason, spider) or response
-
-            rq.mark_request_as_handled(apify_request)
+        # Robots requests are bypassed directly, they don't go through a Scrapy Scheduler, and also through our
+        # Request Queue. Check the scrapy.downloadermiddlewares.robotstxt.RobotsTxtMiddleware for details.
+        if request.url.endswith('robots.txt'):
             return response
 
-        with EventLoopManager() as nested_event_loop:
-            return nested_event_loop.run_until_complete(handle_retry_logic())
+        try:
+            returned = self._event_loop.run_until_complete(self._handle_retry_logic(request, response, spider))
+        except BaseException:
+            traceback.print_exc()
+
+        return returned
+
+    async def _handle_retry_logic(
+        self,
+        request: Request,
+        response: Response,
+        spider: Spider
+    ) -> Request | Response:
+        Actor.log.debug(f'[{get_running_event_loop_id()}] handle_retry_logic is called...')
+        req_id = request.meta.get('apify_request_id')
+        req_unique_key = request.meta.get('apify_request_unique_key')
+
+        apify_request = {
+            'url': request.url,
+            'method': request.method,
+            'id': req_id.decode('utf-8') if isinstance(req_id, bytes) else req_id,
+            'uniqueKey': req_unique_key.decode('utf-8') if isinstance(req_unique_key, bytes) else req_unique_key,
+            # 'retryCount': ..., # todo: where to get this from?
+        }
+
+        if request.meta.get("dont_retry", False):
+            await self._rq.mark_request_as_handled(apify_request)
+            return response
+
+        if response.status in self.retry_http_codes:
+            await self._rq.reclaim_request(apify_request)
+            reason = response_status_message(response.status)
+            return self._retry(request, reason, spider) or response
+
+        await self._rq.mark_request_as_handled(apify_request)
+        return response
