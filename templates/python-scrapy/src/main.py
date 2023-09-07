69038a1138291cc3f85f256e42b509906cb8663d
Vlada Dusek <v.dusek96@gmail.com>
2023-09-07 20:15:43 +0200

    Add get_settings method

--- templates/python-scrapy/src/main.py
+++ templates/python-scrapy/src/main.py
@@ -1,27 +1,38 @@
 from scrapy.crawler import CrawlerProcess
 from scrapy.utils.project import get_project_settings
+from scrapy.settings import Settings
 
 from apify import Actor
 
 from .spiders.title_spider import TitleSpider
 
 
+def get_settings(max_depth: int) -> Settings:
+    """
+    Get Scrapy project settings.
+    """
+    settings = get_project_settings()
+    settings['ITEM_PIPELINES'] = {
+        'src.pipelines.ActorDatasetPushPipeline': 1,
+    }
+    settings['SCHEDULER'] = 'src.scheduler.ApifyScheduler'
+    settings['DOWNLOADER_MIDDLEWARES'] = {
+        'scrapy.downloadermiddlewares.retry.RetryMiddleware': None,
+        'src.middlewares.ApifyRetryMiddleware': 543,
+    }
+    settings['DEPTH_LIMIT'] = max_depth
+    return settings
+
+
 async def main():
     async with Actor:
         Actor.log.info('Actor is being executed...')
 
+        # Process Actor input
         actor_input = await Actor.get_input() or {}
         max_depth = actor_input.get('max_depth', 1)
         start_urls = [start_url.get('url') for start_url in actor_input.get('start_urls', [{'url': 'https://apify.com'}])]
-
-        settings = get_project_settings()
-        settings['ITEM_PIPELINES'] = {'src.pipelines.ActorDatasetPushPipeline': 1}
-        settings['SCHEDULER'] = 'src.scheduler.ApifyScheduler'
-        settings['DOWNLOADER_MIDDLEWARES'] = {
-            'scrapy.downloadermiddlewares.retry.RetryMiddleware': None,
-            'src.middlewares.ApifyRetryMiddleware': 543,
-        }
-        settings['DEPTH_LIMIT'] = max_depth
+        settings = get_settings(max_depth)
 
         # Add start URLs to the request queue
         rq = await Actor.open_request_queue()
