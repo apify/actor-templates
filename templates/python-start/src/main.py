f3867b2af11b409a0be3437d7a03dac8e7f13738
Vlada Dusek <v.dusek96@gmail.com>
2023-10-05 13:59:16 +0200

    python-start: use HTTPX instead of Requests

--- templates/python-start/src/main.py
+++ templates/python-start/src/main.py
@@ -1,9 +1,9 @@
-# Requests - library for making HTTP requests in Python (Read more at https://requests.readthedocs.io)
-import requests
 # Beautiful Soup - library for pulling data out of HTML and XML files (Read more at https://www.crummy.com/software/BeautifulSoup/bs4/doc)
 from bs4 import BeautifulSoup
+# HTTPX - library for making asynchronous HTTP requests in Python (Read more at https://www.python-httpx.org/)
+from httpx import AsyncClient
 
-# Apify SDK - toolkit for building Apify Actors (Read more at https://docs.apify.com/sdk/python).
+# Apify SDK - toolkit for building Apify Actors (Read more at https://docs.apify.com/sdk/python)
 from apify import Actor
 
 
@@ -13,18 +13,20 @@ async def main():
         actor_input = await Actor.get_input() or {}
         url = actor_input.get('url')
 
-        # Fetch the HTML content of the page.
-        response = requests.get(url)
+        # Create an asynchronous HTTPX client
+        async with AsyncClient() as client:
+            # Fetch the HTML content of the page.
+            response = await client.get(url)
 
-        # Parse the HTML content using Beautiful Soup.
+        # Parse the HTML content using Beautiful Soup
         soup = BeautifulSoup(response.content, 'html.parser')
 
-        # Extract all headings from the page (tag name and text).
+        # Extract all headings from the page (tag name and text)
         headings = []
         for heading in soup.find_all(['h1', 'h2', 'h3', 'h4', 'h5', 'h6']):
             heading_object = {'level': heading.name, 'text': heading.text}
-            print('Extracted heading', heading_object)
+            Actor.log.info(f'Extracted heading: {heading_object}')
             headings.append(heading_object)
 
-        # Save headings to Dataset - a table-like storage.
+        # Save headings to Dataset - a table-like storage
         await Actor.push_data(headings)
