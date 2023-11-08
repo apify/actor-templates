2c32fe1509bee3bb5c11ab167447f5d953723fb5
Jindřich Bär <jindrichbar@gmail.com>
2023-11-08 14:05:10 +0100

    feat: conflate the templates/wrappers building codebase

--- src/build.js
+++ src/build.js
@@ -1,11 +1,5 @@
 const { buildTemplates } = require('./build_templates');
-const { buildWrappers } = require('./build_wrappers');
 
-// run build
 (async () => {
-    // build templates
     await buildTemplates();
-    console.log('Template zips were created!');
-    await buildWrappers();
-    console.log('Wrapper zips were created!');
 })();
