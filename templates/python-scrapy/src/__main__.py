e74c02793e721f09147997e0de8362fb9ded6d91
Vlada Dusek <v.dusek96@gmail.com>
2023-11-21 16:01:59 +0100

    Improve docs of the Apify Scrapy template (#229)

--- templates/python-scrapy/src/__main__.py
+++ templates/python-scrapy/src/__main__.py
@@ -1,9 +1,18 @@
-# This file is executed only when the project is run as an Apify Actor,
-# not when you run it locally using `scrapy crawl`.
+"""
+This module transforms a Scrapy project into an Apify Actor, handling the configuration of logging, patching Scrapy's
+logging system, and establishing the required environment to run the Scrapy spider within the Apify platform.
 
-# We need to configure the logging first before we import anything else,
-# so that nothing else imports `scrapy.utils.log` before we patch it.
+This file is specifically designed to be executed when the project is run as an Apify Actor using `apify run` locally
+or being run on the Apify platform. It is not being executed when running the project as a Scrapy project using
+`scrapy crawl`.
+
+We recommend you do not modify this file unless you really know what you are doing.
+"""
+
+# We need to configure the logging first before we import anything else, so that nothing else imports
+# `scrapy.utils.log` before we patch it.
 import logging
+from typing import Any
 import scrapy.utils.log
 from apify.log import ActorLogFormatter
 
@@ -21,21 +30,17 @@ apify_client_logger = logging.getLogger('apify_client')
 apify_client_logger.setLevel(LOGGING_LEVEL)
 apify_client_logger.addHandler(handler)
 
-
-# We can't attach our log handler to the loggers normally,
-# because Scrapy would remove them in the `configure_logging` call here:
-# https://github.com/scrapy/scrapy/blob/a5c1ef82762c6c0910abea00c0a6249c40005e44/scrapy/utils/log.py#L95
-# (even though `disable_existing_loggers` is set to False :facepalm:).
-# We need to monkeypatch Scrapy's `configure_logging` method like this,
-# so that our handler is attached right after Scrapy calls the `configure_logging` method,
-# because otherwise we would lose some log messages.
+# We can't attach our log handler to the loggers normally, because Scrapy would remove them in the `configure_logging`
+# call here: https://github.com/scrapy/scrapy/blob/2.11.0/scrapy/utils/log.py#L113 (even though
+# `disable_existing_loggers` is set to False :facepalm:). We need to monkeypatch Scrapy's `configure_logging` method
+# like this, so that our handler is attached right after Scrapy calls the `configure_logging` method, because
+# otherwise we would lose some log messages.
 old_configure_logging = scrapy.utils.log.configure_logging
 
-def new_configure_logging(*args: tuple, **kwargs: dict) -> None:
+def new_configure_logging(*args: Any, **kwargs: Any) -> None:
     old_configure_logging(*args, **kwargs)
 
-    # Scrapy uses these four main loggers:
-    # https://github.com/scrapy/scrapy/blob/a5c1ef82762c6c0910abea00c0a6249c40005e44/scrapy/utils/log.py#L44-L61
+    # Scrapy uses these four main loggers: https://github.com/scrapy/scrapy/blob/2.11.0/scrapy/utils/log.py#L60:L77
     scrapy_logger = logging.getLogger('scrapy')
     twisted_logger = logging.getLogger('twisted')
     filelock_logger = logging.getLogger('filelock')
@@ -47,14 +52,13 @@ def new_configure_logging(*args: tuple, **kwargs: dict) -> None:
     filelock_logger.addHandler(handler)
     hpack_logger.addHandler(handler)
 
-    # Set levels
+    # Set logging level
     scrapy_logger.setLevel(LOGGING_LEVEL)
     twisted_logger.setLevel(LOGGING_LEVEL)
     filelock_logger.setLevel(LOGGING_LEVEL)
     hpack_logger.setLevel(LOGGING_LEVEL)
 
 scrapy.utils.log.configure_logging = new_configure_logging
-
 
 # Now we can do the rest of the setup
 import asyncio
@@ -63,10 +67,12 @@ import nest_asyncio
 from scrapy.utils.reactor import install_reactor
 from .main import main
 
-# This is necessary so that twisted and asyncio work well together
+# This is necessary so that Twisted and AsyncIO work well together
 install_reactor('twisted.internet.asyncioreactor.AsyncioSelectorReactor')
 nest_asyncio.apply()
 
+# Specify the path to the Scrapy project settings module
 os.environ['SCRAPY_SETTINGS_MODULE'] = 'src.settings'
 
+# Run the Apify main coroutine
 asyncio.run(main())
