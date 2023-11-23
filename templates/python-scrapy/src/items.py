c5fdccf953b6d1b596ae27fc33179d06a30966d9
Vlada Dusek <v.dusek96@gmail.com>
2023-11-23 16:46:59 +0100

    Scrapy template: Minor code update and docs improvement (#236)

--- templates/python-scrapy/src/items.py
+++ templates/python-scrapy/src/items.py
@@ -1,6 +1,12 @@
-# Define the models for your scraped items here
-#
-# See the Scrapy documentation: http://doc.scrapy.org/en/latest/topics/items.html
+"""
+Scrapy item models module
+
+This module defines Scrapy item models for scraped data. Items represent structured data
+extracted by spiders.
+
+For detailed information on creating and utilizing items, refer to the official documentation:
+https://docs.scrapy.org/en/latest/topics/items.html
+"""
 
 from scrapy import Field, Item
 
