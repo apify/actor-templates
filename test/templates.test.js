6ac8dcf495aa882432c62815ffdf313de8fca302
Vlad Frangu <me@vladfrangu.dev>
2024-04-28 13:27:04 +0300

    chore: fix windows (you need to update all spawnSync calls, who would've thunk it)

--- test/templates.test.js
+++ test/templates.test.js
@@ -1,4 +1,4 @@
-const { spawnSync } = require('child_process');
+const { spawnSync: _spawnSync } = require('child_process');
 const fs = require('fs');
 const os = require('os');
 const path = require('path');
@@ -14,11 +14,14 @@ const NPM_COMMAND = /^win/.test(process.platform) ? 'npm.cmd' : 'npm';
 const PYTHON_COMMAND = /^win/.test(process.platform) ? 'python' : 'python3';
 const PYTHON_VENV_COMMAND = /^win/.test(process.platform) ? '.venv\\Scripts\\python.exe' : '.venv/bin/python3';
 const APIFY_COMMAND = /^win/.test(process.platform) ? 'apify.cmd' : 'apify';
+
 const windowsOptions = /^win/.test(process.platform) ? { shell: true, windowsHide: true } : {};
 
-const APIFY_SDK_JS_LATEST_VERSION = spawnSync(NPM_COMMAND, ['view', 'apify', 'version'], {
-    ...process.platform === 'win32' ? windowsOptions : {},
-}).stdout.toString().trim();
+function spawnSync(command, args, options = {}) {
+    return _spawnSync(command, args, { ...options, ...windowsOptions });
+}
+
+const APIFY_SDK_JS_LATEST_VERSION = spawnSync(NPM_COMMAND, ['view', 'apify', 'version']).stdout.toString().trim();
 
 const APIFY_SDK_PYTHON_LATEST_VERSION = spawnSync(PYTHON_COMMAND, ['-m', 'pip', 'index', 'versions', 'apify']).stdout.toString().match(/\((.*)\)/)[1];
 
@@ -116,7 +119,6 @@ const checkPythonTemplate = () => {
 const checkTemplateRun = () => {
     const apifyRunSpawnResult = spawnSync(APIFY_COMMAND, ['run'], {
         env: { ...process.env, APIFY_HEADLESS: '1' },
-        ...process.platform === 'win32' ? windowsOptions : {},
     });
     checkSpawnResult(apifyRunSpawnResult);
 };
