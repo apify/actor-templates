5e12883a0e4a4c4ae0b239ad44e701f4986e1144
Vlada Dusek <v.dusek96@gmail.com>
2024-09-09 12:37:15 +0200

    rm logging conf

--- templates/python-beautifulsoup/src/__main__.py
+++ templates/python-beautifulsoup/src/__main__.py
@@ -1,21 +1,6 @@
 import asyncio
-import logging
-
-from apify.log import ActorLogFormatter
 
 from .main import main
-
-# Configure loggers.
-handler = logging.StreamHandler()
-handler.setFormatter(ActorLogFormatter())
-
-apify_client_logger = logging.getLogger('apify_client')
-apify_client_logger.setLevel(logging.INFO)
-apify_client_logger.addHandler(handler)
-
-apify_logger = logging.getLogger('apify')
-apify_logger.setLevel(logging.DEBUG)
-apify_logger.addHandler(handler)
 
 # Execute the Actor entrypoint.
 asyncio.run(main())
