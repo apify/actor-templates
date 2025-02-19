c72ec1d53f84eb60ff80a5d90bb91e051bd7a161
Jiří Spilka <jiri.spilka@apify.com>
2025-02-19 23:15:53 +0100

    feat: add llama index ppe (#324)

--- templates/python-llamaindex-agent/src/main.py
+++ templates/python-llamaindex-agent/src/main.py
@@ -11,7 +11,7 @@ https://docs.apify.com/sdk/python
 
 from __future__ import annotations
 
-import os
+import math
 from typing import TYPE_CHECKING
 
 from apify import Actor
@@ -21,12 +21,6 @@ from .agent import run_agent
 
 if TYPE_CHECKING:
     from llama_index.core.chat_engine.types import AgentChatResponse
-
-fallback_input = {
-    'query': 'This is fallback test query, do not nothing and ignore it.',
-    'modelName': 'gpt-4o-mini',
-    'llmProviderApiKey': os.getenv('OPENAI_API_KEY'),
-}  # fallback to the OPENAI_API_KEY environment variable when value is not present in the input.
 
 
 async def main() -> None:
@@ -38,20 +32,18 @@ async def main() -> None:
     """
     async with Actor:
         Actor.log.info('Starting LlamaIndex Agent')
+        count = math.ceil((Actor.get_env().get('memory_mbytes', 1024) or 1024) // 1024)
+        await Actor.charge(event_name='actor-start-gb', count=count)
+        Actor.log.info('Charged for Actor start %d GB', count)
         try:
             if not (actor_input := await Actor.get_input()):
                 await Actor.fail(status_message='Actor input was not provided')
                 return
-
-            if not Actor.is_at_home():
-                actor_input = {
-                    **fallback_input,
-                    **actor_input,
-                }  # fallback input is provided only for testing, you need to delete this line
-
             await check_inputs(actor_input)
-            answer = await run_query(actor_input['query'], actor_input['modelName'], actor_input['llmProviderApiKey'])
+            answer = await run_query(actor_input['query'], actor_input['modelName'])
             await Actor.push_data({'query': actor_input['query'], 'answer': answer})
+            Actor.log.info('Charging for task completed')
+            await Actor.charge(event_name='task-completed', count=1)
         except Exception as e:
             await Actor.fail(status_message='Failed to process query', exception=e)
 
@@ -59,20 +51,16 @@ async def main() -> None:
 async def check_inputs(actor_input: dict) -> None:
     """Check that provided input exists.
 
-    :raises Exception: If query or llmProviderApiKey is not provided
+    :raises Exception: If query is not provided
     """
     if not actor_input.get('query'):
         msg = 'Input `query` is not provided. Please verify that the `query` is correctly set.'
         await Actor.fail(status_message=msg)
 
-    if not actor_input.get('llmProviderApiKey'):
-        msg = 'Input `llmProviderApiKey` is not provided. Please verify that the `llmProviderApiKey` is correctly set.'
-        await Actor.fail(status_message=msg)
 
-
-async def run_query(query: str, model_name: str, llm_provider_api_key: str) -> AgentChatResponse | None:
+async def run_query(query: str, model_name: str) -> AgentChatResponse | None:
     """Process query with LlamaIndex Agent."""
-    llm = OpenAI(model=str(model_name), api_key=llm_provider_api_key, temperature=0)
+    llm = OpenAI(model=str(model_name), temperature=0)
     try:
         return await run_agent(query=query, llm=llm, verbose=True)
     except Exception as e:
