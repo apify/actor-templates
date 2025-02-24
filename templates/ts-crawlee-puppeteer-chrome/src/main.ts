6e3e9f261539b8ee990fcd43f61289b6f8f55add
MQ <me@kopecky.io>
2025-02-24 16:59:42 +0100

    add no-sandbox chrome arg to fix failing node template tests

--- templates/ts-crawlee-puppeteer-chrome/src/main.ts
+++ templates/ts-crawlee-puppeteer-chrome/src/main.ts
@@ -26,6 +26,7 @@ const crawler = new PuppeteerCrawler({
         launchOptions: {
             args: [
                 '--disable-gpu', // Mitigates the "crashing GPU process" issue in Docker containers
+                '--no-sandbox', // Mitigates the "sandboxed" process issue in Docker containers
             ]
         }
     }
