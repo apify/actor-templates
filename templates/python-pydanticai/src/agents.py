ef7fca7079712ae8496e936c48e7c2b492c628b1
Josef Procházka <jos.prochazka@post.cz>
2025-03-27 13:20:35 +0100

    feat: Add PydanticAI template (#360)
    Add PydanticAI template
    
    ---------
    
    Co-authored-by: Jiří Spilka <jiri.spilka@apify.com>
    Co-authored-by: Jakub Kopecký <themq37@gmail.com>

---
+++ templates/python-pydanticai/src/agents.py
@@ -0,0 +1,33 @@
+from dataclasses import dataclass
+
+from pydantic_ai import Agent, RunContext, Tool
+
+
+@dataclass
+class Deps:
+    """Dependencies."""
+
+    joke_topic: str
+
+
+async def create_joke(ctx: RunContext[Deps]) -> str:
+    """Create a joke using AI agent."""
+    joker = Agent(
+        'openai:gpt-4o',
+        result_type=str,
+        system_prompt='You are a joke creation agent.',
+    )
+    return await joker.run(user_prompt=ctx.deps.joke_topic)
+
+
+def get_joker_agent() -> Agent:
+    """Get a joke creation agent."""
+    return Agent(
+        'openai:gpt-4o',
+        result_type=str,
+        system_prompt=(
+            'Use `create_joke` tool to create four jokes, select the best one and return it without any other comments.'
+        ),
+        deps_type=Deps,
+        tools=[Tool(create_joke, takes_ctx=True)],
+    )
---
+++ templates/python-pydanticai/src/main.py
@@ -0,0 +1,46 @@
+"""Module defines the main entry point for the Apify Actor.
+
+Feel free to modify this file to suit your specific needs.
+
+To build Apify Actors, utilize the Apify SDK toolkit, read more at the official documentation:
+https://docs.apify.com/sdk/python
+"""
+
+from __future__ import annotations
+
+import os
+
+from apify import Actor
+
+from src.agents import Deps, get_joker_agent
+
+
+async def main() -> None:
+    """Define a main entry point for the Apify Actor."""
+    async with Actor:
+        # Charge for Actor start
+        await Actor.charge('actor-start')
+        Actor.log.info('Starting joke generation agent.')
+
+        # Process inputs
+        actor_input = await Actor.get_input()
+        if openai_api_key := (actor_input.get('openAIApiKey') or os.environ.get('OPENAI_API_KEY')):
+            os.environ['OPENAI_API_KEY'] = openai_api_key
+        else:
+            await Actor.fail(
+                status_message='OpenAI API key is missing. Create issues at the Apify platform to inform the developer'
+            )
+
+        if not (joke_topic := actor_input.get('jokeTopic')):
+            await Actor.fail(status_message='Missing "jokeTopic" attribute in input!')
+
+        # Generate joke
+        joke = (await get_joker_agent().run(user_prompt='Tell me a joke.', deps=Deps(joke_topic=joke_topic))).data
+        Actor.log.info(f'The AI generated joke about {joke_topic}:\n{joke}')
+
+        # Store the joke
+        dataset = await Actor.open_dataset()
+        await dataset.push_data({'Topic': joke_topic, 'Joke': joke})
+
+        # Charge for task completion
+        await Actor.charge('task-completed')
