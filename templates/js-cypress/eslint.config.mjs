f64f9b3f1916ea6c377e952f4d03e2645bb67159
Jiří Moravčík <jiri.moravcik@gmail.com>
2025-05-27 13:56:41 +0200

    rest of the JS templates

--- templates/js-cypress/eslint.config.mjs
+++ templates/js-cypress/eslint.config.mjs
@@ -1,10 +1,12 @@
 import prettier from 'eslint-config-prettier';
+import pluginCypress from 'eslint-plugin-cypress/flat'
 
 import apify from '@apify/eslint-config/js.js';
 
 // eslint-disable-next-line import/no-default-export
 export default [
     { ignores: ['**/dist'] },
+    pluginCypress.configs.recommended,
     ...apify,
     prettier,
 ];
